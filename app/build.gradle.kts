dcdf7d8825c90ec6a6cf12d7bfc50ae8c90236fd
Lazy Bill <lazybill2580@gmail.com>
2025-10-04 09:20:29 +0500

    Правки 3АТГ фВ(спиннер)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.ps_inspection"
         minSdk = 26
         targetSdk = 36
-        versionCode = 21
-        versionName = "1.0.4"
+        versionCode = 22
+        versionName = "1.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
