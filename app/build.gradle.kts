32356472e2c41743d69c0dd0f6f66e0438b56f1a
Lazy Bill <lazybill2580@gmail.com>
2026-05-06 21:48:39 +0500

    t н.в. онлайн, с сохранением в осмотре excell

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 36
         versionCode = 24
-        versionName = "2.3.7"
+        versionName = "2.3.8"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
