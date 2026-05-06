c9ee26e9dab6d808c0bffe14c78ac720e45a0e70
Lazy Bill <lazybill2580@gmail.com>
2026-05-07 00:22:06 +0500

    Список дежурных + заполнение в excell

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,4 +75,9 @@ dependencies {
     implementation("com.github.chrisbanes:PhotoView:2.3.0") {
         exclude(group = "androidx.appcompat", module = "appcompat")
     }
+
+    // Google Sheets API
+    implementation("com.google.api-client:google-api-client-android:2.2.0")
+    implementation("com.google.apis:google-api-services-sheets:v4-rev20230815-2.0.0")
+    implementation("com.google.auth:google-auth-library-oauth2-http:1.19.0")
 }
